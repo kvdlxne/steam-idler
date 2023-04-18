87f234468c626b02718c51e11237544fd0508f09
kvdlxne <kvdlxne@gmail.com>
2023-04-18 18:49:45 +0200

    Changed the way the application reads a steam application identifier.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 #![windows_subsystem = "windows"]
 
-use std::thread;
+use std::{env, process, thread};
 use std::time::Duration;
-use steamworks::Client;
+use steamworks::{AppId, Client};
 
 fn main() {
-  Client::init().unwrap();
+  let args: Vec<String> = env::args().skip(1).collect();
+  if 1 > args.len() {
+    println!("The application needs 1 argument.");
+    process::exit(0);
+  }
+
+  let app_id = match args[0].parse::<u32>() {
+    Err(_) => {
+      println!("Invalid number!");
+      process::exit(0);
+    }
+    Ok(it) => AppId::from(it)
+  };
+
+  Client::init_app(app_id).unwrap();
   loop {
     thread::sleep(Duration::from_secs(60));
   }
